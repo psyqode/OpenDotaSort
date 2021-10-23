8e94f51f45968eceea621785b532a7933c28f993
Jake Wilkins <jacob.n.wilkins@gmail.com>
2021-10-22 22:21:44 -0600

    fetching mo data, mutating mo shit

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-const fetchRoleData = async () => {
-  const data = await fetch('https://api.opendota.com/api/scenarios/laneRoles?lane_role=3')
-    .then((resp) => resp.json())
+let laneRoleData
+let heroData
 
+const calculateWinPercentage = (rawData) => {
+  for (let i = 0; i < rawData.length; i++) {
+    const games = rawData[i].games
+    const wins = rawData[i].wins
 
-  document.body.innerHTML = JSON.stringify(data)
+    const winPercentage = wins/games
+
+    rawData[i].winPercentage = winPercentage
+  }
+
+  return rawData
 }
 
-fetchRoleData()
+
+const fetchRoleData = async () => {
+  const data = await fetch('https://api.opendota.com/api/scenarios/laneRoles')
+    .then((resp) => resp.json())
+
+  const dataWithWinPercentage = calculateWinPercentage(data)
+
+  laneRoleData = dataWithWinPercentage;
+}
+
+
+const fetchHeroData = async () => {
+  const data = await fetch('https://api.opendota.com/api/heroes')
+    .then((resp) => resp.json())
+
+  heroData = data;
+}
+
+
+const setup = () => {
+  fetchRoleData()
+  fetchHeroData()
+}
+
+setup()
